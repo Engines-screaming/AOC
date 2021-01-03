633f22cc75a874f7b7448ecf7a79d3b9dce0a52a
kravago3 <kravago3@gatech.edu>
2021-01-02 19:11:39 -0800

    day 4 part 1 solution

--- day_4/day_4_solution.py
+++ day_4/day_4_solution.py
@@ -11,10 +11,51 @@ The automatic passport scanners are slow because they're having trouble detectin
 - cid (Country ID)
 '''
 
-# read in inputs
+VERBOSE = True  # debugging
 
-with open('input.txt') as f:
+# read in inputs
+# input_file = 'test_input.txt'
+input_file = 'input.txt'
+
+with open(input_file) as f:
     input_txt = f.read()
 
-documents = input_txt.split('\n\n')
-print(documents[0])
+documents = input_txt.split('\n\n')  # put each doc in a list
+
+# make a list of dicts. each dict is key value pairs of fields in doc. 
+parsed_docs = []
+for doc in documents:
+    doc_dict = {}
+    doc = doc.replace('\n', ' ')
+    kv_pairs = doc.split(' ')
+
+    for pair in kv_pairs:
+        k, v = pair.split(':')
+        doc_dict[k] = v
+    
+    parsed_docs.append(doc_dict)
+    
+# check for validity
+check_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
+valid_count = 0
+invalid_count = 0
+for doc in parsed_docs:
+    valid = True
+    for field in check_fields:
+        if field not in doc:
+            valid = False
+            
+            if VERBOSE:
+                print(f"key not in check field: {field}")
+
+    if valid == False:
+        invalid_count += 1
+    else:
+        valid_count += 1
+        
+        if VERBOSE:
+            print(f'dict not valid:{doc}\n')
+
+print(f'The amount of valid passports: {valid_count}.\nInvalid passports: {invalid_count}')
+    
